a67da25d83fa2443b3603897aecbd6733cf91caf
Maxime Lefebvre <lefebvremaxime00@gmail.com>
2021-10-26 18:01:59 +0200

    create functions to get API elements & functions to display API elements

--- front/html/app.js
+++ front/html/app.js
@@ -1,44 +1,36 @@
+let baseHrefUrl = 'article.html?id=';
+
+let url = window.location;
+let params = (new URL(url)).searchParams;
+
+// let url = 'https://www.example.com?name=a,b,c';
+// let params = (new URL(url)).searchParams;
+// // console.log(window.location);
+// console.log('id ====', params.get('name').split(','));
 
 
-function getProducts() {
-    fetch('http://localhost:3000/api/furniture')
-    .then(res => {
-        if(res.ok) {
-             res.json().then(data => {
-                
-                 for (i = 0; i < data.length; i++) {
+function displayProducts(res) {
+    let card = document.createElement('div');
+    card.classList.add('catalogListItem');
+    document.getElementById('catalogList').appendChild(card);
 
-                    let card = document.createElement('div');
-                    card.classList.add('catalogListItem');
-                    document.getElementById('catalogList').appendChild(card);
+    let name = document.createElement('p');
+    name.classList.add('catalogListItemName');
+    name.innerHTML = res.name;
+    card.appendChild(name);
 
-                    let name = document.createElement('p');
-                    name.classList.add('catalogListItemName');
-                    name.innerHTML = data[i].name;
-                    card.appendChild(name);
+    let img = document.createElement('img');
+    img.classList.add('catalogListItemImg');
+    img.setAttribute('width', '70%');
+    img.src = res.imageUrl;
+    card.appendChild(img)
 
-                    let img = document.createElement('img');
-                    img.classList.add('catalogListItemImg');
-                    img.setAttribute('width', '70%');
-                    img.src = data[i].imageUrl;
-                    card.appendChild(img)
-
-                    let moreInfo = document.createElement('a');
-                    moreInfo.classList.add('catalogListItemMore');
-                    moreInfo.innerText = `Plus d'info`;
-                    card.appendChild(moreInfo);   
-                 }
-                  
-             })
-        } else {
-            console.log('Error');
-            document.getElementById('error').innerHTML = "Erreur";
-        }
-    })
+    let moreInfo = document.createElement('a');
+    moreInfo.classList.add('catalogListItemMore');
+    moreInfo.setAttribute('href', baseHrefUrl + res._id);
+    moreInfo.innerText = `Plus d'info`;
+    card.appendChild(moreInfo);
 }
-
-
-
 
 
 
@@ -47,8 +39,10 @@ function getProducts() {
     .then(res => {
         if(res.ok) {
             res.json().then(data => {
-                return data
-             })
+                for(let i=0; i<data.length; i++) {
+                    displayProducts(data[i])
+                }
+            });
         } else {
             console.log('Error');
             document.getElementById('error').innerHTML = "Erreur";
@@ -56,36 +50,62 @@ function getProducts() {
     })
 }
 
+getProducts()
 
 
+function displayIdProduct(res){
+    let articleDisplay = document.querySelector('.displayArticle');
+    let articleDisplayText = document.querySelector('.displayArticleTxt');
+    let articleInfos = document.querySelector('.displayArticleInfo');
+    let descriptionContainer = document.querySelector('.informationsDescription');
+    let colorList = document.querySelector('.informationsColors');
 
+    let img = document.querySelector('.articleImg');
+    img.setAttribute('src', res.imageUrl);
+    articleDisplay.appendChild(img);
 
-// function listTemplate() {
-//     let template = document.createElement('div');
-//     template.innerHTML = `<div class="catalogListItem">
-//                           <a href="{{{urlPage}}}"> <p class="catalogListItemName"></p>{{{name}}}</a> 
-//                           <a href="{{{urlPage}}}" class="catalogListItemImgLink"> 
-//                           <img class="catalogListItemImg" src="{{{urlImg}}}" alt="" width="70%"> </a>
-//                           <a href="{{{urlPage}}}" class="catalogListItemMore" > Plus d'infos </a>
-//                       </div>`
-//     return template;
-// }
+    let name = document.createElement('p');
+    name.classList.add('displayArticleInfoName');
+    name.innerText = res.name;
+    articleInfos.appendChild(name);
 
-function generateDOM(data){
-    getProducts();
-    listTemplate().replace("{{{name}}}", data.name);
-    listTemplate().replace("{{{urlPage}}}", data.id);
-    listTemplate().replace("{{{urlImg}}}", data.urlImage);
+    let price = document.createElement('p');
+    price.classList.add('displayArticleInfoPrice');
+    price.innerText = (res.price/100) + ' €';
+    articleInfos.appendChild(price);
+
+    let cartAdd = document.createElement('a');
+    cartAdd.classList.add('displayArticleBtn');
+    cartAdd.innerText = 'Ajouter au panier';
+    articleDisplayText.appendChild(cartAdd);
+
+    let description = document.querySelector('.informationsDescriptionDesc');
+    description.innerText = res.description;
+    descriptionContainer.appendChild(description);
+
+    for(i = 0;i < res.varnish.length; i++){
+        let color = document.createElement('div');
+        color.style.backgroundColor = res.varnish[i];
+        color.classList.add('informationsColorsColor');
+        colorList.appendChild(color);
+        console.log(res.varnish[i]);
+    }
+
+    
 }
 
-// function createCard(){
-//     let card = document.getElementById('catalogList').appendChild(listTemplate());
-//     card.generateDOM();
+function getIdProduct() {
+    fetch(`http://localhost:3000/api/furniture/${params.get('id')}`)
+    .then(res => {
+        if(res.ok) {
+            res.json().then(data => {
+                displayIdProduct(data);
+            });
+        } else {
+            console.log('Error');
+            document.getElementById('error').innerHTML = "Erreur";
+        }
+    })
+}
 
-// }
-
-// data.forEach(createCard());
-
-
-// console.log(listTemplate());
-
+getIdProduct();
